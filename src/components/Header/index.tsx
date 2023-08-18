8ef3fc9c20830f9d2c1e71eb12c85ab667fb26c2
Luancss <ventobo123@gmail.com>
2023-08-18 01:17:31 -0300

    Component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content} from './styles'
+
+export function Header () {
+  return (
+   <Container>
+    <Content>
+    <img src={logoImg} alt="dt money" />
+    <button type="button">
+      Nova Transação
+    </button>
+    </Content>
+   </Container>
+  )
+}
+
+export default Header
