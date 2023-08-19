5cecba3bff90811bcdbd3fa4d5b7c1de11edc135
Luancss <ventobo123@gmail.com>
2023-08-19 03:08:57 -0300

    Component TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionsTable";
 
 export function Dashboard () {
   return (
     <Container>
       <Summary/>
+      <TransactionTable/>
     </Container>
   )
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+
+export function TransactionTable () {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de website</td>
+              <td className="deposit">R$12.000</td>
+              <td>Desenvolvimento</td>
+              <td>20/02/2023</td>
+            </tr>
+            <tr>
+              <td>Aluguel</td>
+              <td className="withdraw">- R$1.100</td>
+              <td>Casa</td>
+              <td>10/02/2023</td>
+            </tr>
+          </tbody>
+  
+      </table>
+    </Container>
+  )
+}
