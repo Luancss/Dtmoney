44c673037d9a4e5371ddff33c517e698afc4482a
Luancss <ventobo123@gmail.com>
2023-08-21 20:13:13 -0300

    Configurando modal de criação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
-import logoImg from '../../assets/logo.svg'
-import { Content, Container } from './styles'
+import { useState } from "react";
+import logoImg from "../../assets/logo.svg";
+import { Content, Container } from "./styles";
 
-export function Header () {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
   return (
-   <Container>
-    <Content>
-    <img src={logoImg} alt="dt money" />
-    <button type="button">
-      Nova Transação
-    </button>
-    </Content>
-   </Container>
-  )
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
+       
+      </Content>
+    </Container>
+  );
 }
